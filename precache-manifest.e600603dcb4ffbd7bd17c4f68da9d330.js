c6e7b845fe9c36d7c6dbd3444c4188b134e056fe
olessiap <olessia.potapova@gmail.com>
2020-01-21 22:04:31 -0300

    Build files for GitHub pages

---
+++ precache-manifest.e600603dcb4ffbd7bd17c4f68da9d330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8945860606e7a2eba3768bf4cc44756",
+    "url": "/GithubTopStarredRepositories/index.html"
+  },
+  {
+    "revision": "370f5da5b8a4d335b6d9",
+    "url": "/GithubTopStarredRepositories/static/css/main.5a50f497.chunk.css"
+  },
+  {
+    "revision": "d7d953ffee0018c0e450",
+    "url": "/GithubTopStarredRepositories/static/js/2.9dd1c8d4.chunk.js"
+  },
+  {
+    "revision": "370f5da5b8a4d335b6d9",
+    "url": "/GithubTopStarredRepositories/static/js/main.cdf4213d.chunk.js"
+  },
+  {
+    "revision": "03c38758708c7a2516fb",
+    "url": "/GithubTopStarredRepositories/static/js/runtime-main.d06725b6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GithubTopStarredRepositories/precache-manifest.e600603dcb4ffbd7bd17c4f68da9d330.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GithubTopStarredRepositories/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
